d76555956327c9099644eeea329238efed9eacfe
Kimsw-G <kimsw3445@naver.com>
2021-04-21 16:43:48 +0900

    just testing

--- java/imsi/Imsi.java
+++ java/imsi/Imsi.java
@@ -3,11 +3,21 @@ package imsi;
 
 public class Imsi{
     public static void main(String[] args)  {
-        String str = "hello";
-        System.out.println(str);
+        int[] arr = new int[10];
+        long num = 1613461572;
+
+        int length = (int)(Math.log10(num)+1);
+
+        for(int i=0;i<length;i++){
+            int temp = (int)(num%10); // 가장 첫자리 저장
+            num /= 10; // 가장 앞자리 죽이기
+            arr[temp]++;
+        }
+
+        for (int i : arr) {
+            System.out.println(i);
+        }
     }
+
 }
 
-class Yes{
-    private final int MAX_INT=0;
-}
